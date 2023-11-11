1b633e0474db120e6191d4f4253b6856c7b29ad5
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-11-11 15:50:30 -0700

    Add challenges instructions

---
+++ problems_solved.rb
@@ -0,0 +1,38 @@
+# Challenge 1
+# Write a method or function that accepts a string. 
+# The method or function should return a boolean that describes 
+# whether or not the string has an even number of characters.
+
+# Challenge 2
+# Update your previous method or function so it accepts an array. 
+# The method or function should return a boolean that describes 
+# whether or not the array's length is an even number.
+
+# REFLECTION
+# What do you like about this process?
+# What might make this process challenging?
+# Are there any steps you would add or modify?
+
+#Challenge 3
+# Write a method or function that accepts a number from 1-24 that 
+# represents the current time rounted to the closest hour in 24-hour 
+# military time. (i.e. 11 = 11:00 AM, 16 = 4:00 PM). The function 
+# should return a different greeting based on the time of day.
+
+# 4 AM - 11 AM: Good morning!
+# 12 PM - 4 PM: Good afternoon!
+# 5 PM - 8PM: Good evening!
+# 9 PM - 3 AM: Good night!
+
+# Challenge 4 - OPTIONAL! Spicy!
+# Write a method or function that accepts an array of 10 integers 
+# (between 0 and 9) and returns a string of those numbers in the form 
+# of a phone number. For example, if you were given 
+# [5, 5, 5, 1, 2, 3, 4, 5, 6, 7], the method or function will 
+# return 555-123-4567.
+
+# Challenge 5 - OPTIONAL! Super Spicy!
+# Start with the array of strings below. Print all of the words in the 
+# array, but change every t to an uppercase T.
+
+# Starter array: ['cat', 'dog', 'parrot', 'turtle', 'bird']

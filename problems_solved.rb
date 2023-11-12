d5d371e4eaf93d71f8beffeda927aafd7001b53a
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-11-11 23:02:32 -0700

    Detail and execute the problem solving methodology phases

--- problems_solved.rb
+++ problems_solved.rb
@@ -2,6 +2,25 @@
 # Write a method or function that accepts a string. 
 # The method or function should return a boolean that describes 
 # whether or not the string has an even number of characters.
+    # 1) *Restate Goal* Write method that evaluates if the number
+    #     of characters in the string value is even and returns T or F.
+    # 2) *Consider Data* The parameter is a string, argument an integer
+    #      that contains number of characters of the parameter which is
+    #      evaluated if it is an even number (T) or not (F).
+    # 3) *Clarifying Questions* Will the methods of: counting characters
+    #     in a string, and determine if number is even exist?
+    # 4) *Break it down* Write a method. Parameter is a string. Call a 
+    #      method on the string to find the number of characters. Asign this
+    #      integer to a variable and call a method on the integer to find and 
+    #      return if it is true or false it is an integer.
+    # 5) *Research Google Ruby method number of characters on a string, Google
+    #      Ruby method evaluate if integer is even or odd.
+    # 6)
+    def find_numcharacters_even_streetaddress (street_address)
+        number_characters = street_address.chars.count
+        puts "Is the number of characters of the street address an even number? #{number_characters.even?}."
+    end
+    
 
 # Challenge 2
 # Update your previous method or function so it accepts an array. 
